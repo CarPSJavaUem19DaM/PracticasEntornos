7bd658598ab3ac914204ea0d1c3b5a6f582e4620
20624677 <20624677@N205-06.alumnos.uem.es>
2019-01-16 10:40:03 +0100

    fffff

---
+++ src/prueba/ClaseGitHub.java
@@ -0,0 +1,10 @@
+package prueba;
+
+public class ClaseGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Primer repositorio para GitHub" + "Creado por CarlosP");
+
+	}
+
+}
