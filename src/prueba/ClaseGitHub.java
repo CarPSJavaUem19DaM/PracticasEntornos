e937f836106eae251a868e080a3525e783b45e21
20624677 <20624677@N205-06.alumnos.uem.es>
2019-01-16 10:43:40 +0100

    modificación con otro sysout

--- src/prueba/ClaseGitHub.java
+++ src/prueba/ClaseGitHub.java
@@ -4,6 +4,7 @@ public class ClaseGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Primer repositorio para GitHub" + "Creado por CarlosP");
+		System.out.println("con modificación");
 
 	}
 
