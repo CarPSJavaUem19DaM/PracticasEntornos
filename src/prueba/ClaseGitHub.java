949355724b23d509b81936212466721b149572a3
21856608 <21856608@N205-07.alumnos.uem.es>
2019-01-16 11:08:20 +0100

    cambio realizado

--- src/prueba/ClaseGitHub.java
+++ src/prueba/ClaseGitHub.java
@@ -5,7 +5,8 @@ public class ClaseGitHub {
 	public static void main(String[] args) {
 		System.out.println("Primer repositorio para GitHub" + "Creado por CarlosP");
 		System.out.println("con modificación");
-		System.out.println("Cambio Realizado por Martin");
+		System.out.println("Primer cambio realizado por Juan");// J.J.
+
 	}
 
 }
