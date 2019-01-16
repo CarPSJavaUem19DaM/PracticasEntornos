48199ef28d142b47313848dbb04a79143913fe7e
Martín <Martn@DESKTOP-GMU97KT>
2019-01-16 11:05:32 +0100

    Cambio Martin

--- src/prueba/ClaseGitHub.java
+++ src/prueba/ClaseGitHub.java
@@ -5,7 +5,7 @@ public class ClaseGitHub {
 	public static void main(String[] args) {
 		System.out.println("Primer repositorio para GitHub" + "Creado por CarlosP");
 		System.out.println("con modificación");
-
+		System.out.println("Cambio Realizado por Martin");
 	}
 
 }
